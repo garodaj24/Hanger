00b4b59e38fe957f1971a62f03e6e5e3f06fb8a3
garod <garod.ajemyan@thecoderiders.com>
2021-10-08 17:39:44 +0400

    deploy

---
+++ js/chunk-2d222708.3cfacf62.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d222708"],{cf2a:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("This is the order page")])},r=[],s={},u=s,a=t("2877"),l=Object(a["a"])(u,c,r,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d222708.3cfacf62.js.map
---
+++ precache-manifest.b9d96a2ccfe4155bed23c68c9044e729.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff942dadec1c74f806c6",
+    "url": "/Hanger/css/app.425a2238.css"
+  },
+  {
+    "revision": "55be088782e845da6b99",
+    "url": "/Hanger/css/chunk-vendors.a2779d31.css"
+  },
+  {
+    "revision": "6d16178811cd7c5ebcde8532caba7083",
+    "url": "/Hanger/index.html"
+  },
+  {
+    "revision": "ff942dadec1c74f806c6",
+    "url": "/Hanger/js/app.d5e57793.js"
+  },
+  {
+    "revision": "8b9f1831def0f1ecb3a4",
+    "url": "/Hanger/js/chunk-2d0d6cf7.927367b3.js"
+  },
+  {
+    "revision": "4da7cecb139a80d7830a",
+    "url": "/Hanger/js/chunk-2d222708.3cfacf62.js"
+  },
+  {
+    "revision": "a3578abb550820883996",
+    "url": "/Hanger/js/chunk-2d22d746.a61a14bf.js"
+  },
+  {
+    "revision": "55be088782e845da6b99",
+    "url": "/Hanger/js/chunk-vendors.bd324b67.js"
+  },
+  {
+    "revision": "049de32a929c13ab9c2833005454d359",
+    "url": "/Hanger/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Hanger/robots.txt"
+  }
+]);
